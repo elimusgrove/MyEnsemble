062872a4085493610ab43544b34867036b7458cb
GaJin <gajin2000@gmail.com>
2020-01-25 21:39:08 -0600

    asdf

---
+++ includes/footer.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<title>W3.CSS Template</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+<body>
+<div class="w3-black" id="footer">
+    <footer class="w3-container w3-padding-64 w3-center w3-opacity w3-xlarge">
+        <p class="w3-medium">Submission for MinneHacks 2020</p>
+    </footer>
+</div>
+</body>

5e655e8a2bb0b767a67d606a48555ffd534ee90b
GaJin <gajin2000@gmail.com>
2020-01-25 21:00:24 -0600

    asdf

--- Index.php
+++ Index.php
@@ -1,9 +1,11 @@
-<!-- <?php
+<?php
 
 // // Establish Database connection
 // $conn = new mysqli($servername, $username, $password);
 
-?> -->
+
+
+?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -16,9 +18,9 @@
 <body>
 
 
+<?php require '/includes/header.php'; ?>
 <!-- Navbar -->
-<?php include Header.php ?>
-<div class="w3-top">
+<!-- <div class="w3-top">
   <div class="w3-bar w3-black w3-card">
     <a class="w3-bar-item w3-button w3-padding-large w3-hide-medium w3-hide-large w3-right" href="javascript:void(0)" onclick="myFunction()" title="Toggle Navigation Menu"><i class="fa fa-bars"></i></a>
     <a href="#" class="w3-bar-item w3-button w3-padding-large">HOME</a>
@@ -29,12 +31,12 @@
       <button class="w3-padding-large w3-button" title="More">USER <i class="fa fa-caret-down"></i></button>
       <div class="w3-dropdown-content w3-bar-block w3-card-4">
         <a href="mysubmissions.php" class="w3-bar-item w3-button">My Submissions</a>
-        <a href="header.php" class="w3-bar-item w3-button">Upload New Music</a>
+        <a href="includes/header.php" class="w3-bar-item w3-button">Upload New Music</a>
       </div>
     </div>
     <a href="javascript:void(0)" class="w3-padding-large w3-hover-red w3-hide-small w3-right"><i class="fa fa-search"></i></a>
   </div>
-</div>
+</div> -->
 
 <!-- Navbar on small screens (remove the onclick attribute if you want the navbar to always show on top of the content when clicking on the links) -->
 <div id="navDemo" class="w3-bar-block w3-black w3-hide w3-hide-large w3-hide-medium w3-top" style="margin-top:46px">

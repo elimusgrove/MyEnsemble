f32669bf7d08ef5a90b2b10189a8bc2088f620e1
Eli Musgrove <eli.musgrove9@gmail.com>
2020-01-25 20:52:56 -0600

    Updates database connection credentials to use google cloud.
    Signed-off-by: Eli Musgrove <eli.musgrove9@gmail.com>

--- includes/db_cred.php
+++ includes/db_cred.php
@@ -1,6 +1,6 @@
 <?php
 
 // Include configuration file for database username and password
-$hostname = 'ksn.kwa.mybluehost.me';
-$username = 'hsnkwamy_ensmbl';
+$hostname = '35.225.154.79';
+$username = 'myensemble';
 $password = 's#!zD7%ESMvrLQqgPZ^6';

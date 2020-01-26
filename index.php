20dba6ba50f18fff6e4a05a54b4b88c4f80ecc47
GaJin <gajin2000@gmail.com>
2020-01-25 21:15:44 -0600

    asdf

--- index.php
+++ index.php
@@ -18,9 +18,8 @@
 <body>
 
 
-<?php require '/includes/header.php'; ?>
 <!-- Navbar -->
-<!-- <div class="w3-top">
+<div class="w3-top">
   <div class="w3-bar w3-black w3-card">
     <a class="w3-bar-item w3-button w3-padding-large w3-hide-medium w3-hide-large w3-right" href="javascript:void(0)" onclick="myFunction()" title="Toggle Navigation Menu"><i class="fa fa-bars"></i></a>
     <a href="#" class="w3-bar-item w3-button w3-padding-large">HOME</a>
@@ -36,7 +35,7 @@
     </div>
     <a href="javascript:void(0)" class="w3-padding-large w3-hover-red w3-hide-small w3-right"><i class="fa fa-search"></i></a>
   </div>
-</div> -->
+</div>
 
 <!-- Navbar on small screens (remove the onclick attribute if you want the navbar to always show on top of the content when clicking on the links) -->
 <div id="navDemo" class="w3-bar-block w3-black w3-hide w3-hide-large w3-hide-medium w3-top" style="margin-top:46px">
--- mysubmissions.php
+++ mysubmissions.php
@@ -6,9 +6,36 @@
 ?>
 
 <!DOCTYPE html>
-<html>
+<html lang="en">
+<title>W3.CSS Template</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+
 <body>
 
+<div class="w3-top">
+    <div class="w3-bar w3-black w3-card">
+        <a class="w3-bar-item w3-button w3-padding-large w3-hide-medium w3-hide-large w3-right" href="javascript:void(0)" onclick="myFunction()" title="Toggle Navigation Menu"><i class="fa fa-bars"></i></a>
+        <a href="#" class="w3-bar-item w3-button w3-padding-large">HOME</a>
+        <a href="#band" class="w3-bar-item w3-button w3-padding-large w3-hide-small">LOG IN</a>
+        <a href="#tour" class="w3-bar-item w3-button w3-padding-large w3-hide-small">REGISTER</a>
+        <a href="#contact" class="w3-bar-item w3-button w3-padding-large w3-hide-small">UPLOAD</a>
+        <div class="w3-dropdown-hover w3-hide-small">
+            <button class="w3-padding-large w3-button" title="More">USER <i class="fa fa-caret-down"></i></button>
+            <div class="w3-dropdown-content w3-bar-block w3-card-4">
+                <a href="mysubmissions.php" class="w3-bar-item w3-button">My Submissions</a>
+                <a href="includes/header.php" class="w3-bar-item w3-button">Upload New Music</a>
+            </div>
+        </div>
+        <a href="javascript:void(0)" class="w3-padding-large w3-hover-red w3-hide-small w3-right"><i class="fa fa-search"></i></a>
+    </div>
+</div>
+
+<div class="w3-content" style="max-width:2000px;margin-top:46px">
 <ul class="mdc-list mdc-list--two-line">
   <li class="mdc-list-item" tabindex="0">
     <span class="mdc-list-item__text">
@@ -29,6 +56,6 @@
     </span>
   </li>
 </ul>
-
+</div>
 </body>
 </html>

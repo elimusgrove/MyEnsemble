2a58321aecfc1bd0ad6f33e22e592bf64851ffe7
David Imhoff <dbimhoff@iastate.edu>
2020-01-25 22:53:36 -0600

    link

--- header.php
+++ header.php
@@ -4,7 +4,7 @@
 <div class="w3-top">
   <div class="w3-bar w3-black w3-card">
     <a class="w3-bar-item w3-button w3-padding-large w3-hide-medium w3-hide-large w3-right" href="javascript:void(0)" onclick="myFunction()" title="Toggle Navigation Menu"><i class="fa fa-bars"></i></a>
-    <a href="#" class="w3-bar-item w3-button w3-padding-large">HOME</a>
+    <a href="index.php" class="w3-bar-item w3-button w3-padding-large">HOME</a>
     <a href="#band" class="w3-bar-item w3-button w3-padding-large w3-hide-small">LOG IN</a>
     <a href="#tour" class="w3-bar-item w3-button w3-padding-large w3-hide-small">REGISTER</a>
     <a href="#contact" class="w3-bar-item w3-button w3-padding-large w3-hide-small">UPLOAD</a>

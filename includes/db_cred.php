d622e3dddea877be6254e8fde1a4648f10594201
Eli Musgrove <43040483+elimusgrove@users.noreply.github.com>
2020-01-29 13:10:44 -0600

    Update db_cred.php

--- includes/db_cred.php
+++ includes/db_cred.php
@@ -1,6 +1,6 @@
 <?php
 
 // Include configuration file for database username and password
-$hostname = '35.225.154.79';
-$username = 'myensemble';
-$password = 's#!zD7%ESMvrLQqgPZ^6';
+$hostname = '';
+$username = '';
+$password = '';
